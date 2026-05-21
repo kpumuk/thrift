ba48140e66a4d1eb3f942bf922cb96f9ddccee89
Dmytro Shteflyuk <kpumuk@apache.org>
2026-05-21 18:32:25 -0400

    Ruby HTTP server must return immediately on oneway RPC call

--- lib/rb/lib/thrift/server/thin_http_server.rb
+++ lib/rb/lib/thrift/server/thin_http_server.rb
@@ -18,6 +18,7 @@
 # under the License.
 #
 
+require 'delegate'
 require 'rack'
 require 'thin'
 
@@ -51,6 +52,8 @@ module Thrift
     class RackApplication
 
       THRIFT_HEADER = "application/x-thrift"
+      RESPONSE_HEADERS = {'Content-Type' => THRIFT_HEADER}.freeze
+      ONEWAY_RESPONSE_HEADERS = RESPONSE_HEADERS.merge('Content-Length' => '0').freeze
 
       def self.for(path, processor, protocol_factory)
         Rack::Builder.new do
@@ -61,7 +64,12 @@ module Thrift
             run lambda { |env|
               request = Rack::Request.new(env)
               if RackApplication.valid_thrift_request?(request)
-                RackApplication.successful_request(request, processor, protocol_factory).finish
+                if RackApplication.async_request?(env)
+                  RackApplication.process_async_request(request, processor, protocol_factory, env['async.callback'])
+                  throw :async
+                end
+
+                RackApplication.successful_request(request, processor, protocol_factory).response.finish
               else
                 RackApplication.failed_request.finish
               end
@@ -70,20 +78,114 @@ module Thrift
         end
       end
 
-      def self.successful_request(rack_request, processor, protocol_factory)
-        response = Rack::Response.new([], 200, {'Content-Type' => THRIFT_HEADER})
-        transport = IOStreamTransport.new rack_request.body, response
-        protocol = protocol_factory.get_protocol transport
+      def self.async_request?(env)
+        env['async.callback']
+      end
+
+      def self.process_async_request(rack_request, processor, protocol_factory, callback)
+        Thread.new do
+          result = nil
+          result = successful_request(rack_request, processor, protocol_factory, callback)
+          async_callback(callback, result.response.finish) unless result.response_sent?
+        rescue StandardError
+          unless result && result.response_sent?
+            async_callback(callback, [500, {'Content-Type' => 'text/plain', 'Content-Length' => '21'}, ['Internal Server Error']])
+          end
+        end
+      end
+
+      def self.async_callback(callback, response)
+        if defined?(EM) && EM.reactor_running?
+          EM.schedule { callback.call(response) }
+        else
+          callback.call(response)
+        end
+      end
+
+      def self.successful_request(rack_request, processor, protocol_factory, async_callback = nil)
+        response = Rack::Response.new([], 200, RESPONSE_HEADERS)
+        result = RequestResult.new(response)
+        transport = ResponseTransport.new(rack_request.body, response)
+        protocol = OnewayResponseProtocol.new(protocol_factory.get_protocol(transport)) do
+          unless result.response_sent?
+            result.mark_response_sent!
+            transport.response_sent!
+            if async_callback
+              RackApplication.async_callback(async_callback, [200, ONEWAY_RESPONSE_HEADERS, []])
+            else
+              response.status = 200
+              response.headers.replace(ONEWAY_RESPONSE_HEADERS)
+              response.body = []
+            end
+          end
+        end
         processor.process protocol, protocol
-        response
+        result
       end
 
       def self.failed_request
-        Rack::Response.new(['Not Found'], 404, {'Content-Type' => THRIFT_HEADER})
+        Rack::Response.new(['Not Found'], 404, RESPONSE_HEADERS)
       end
 
       def self.valid_thrift_request?(rack_request)
         rack_request.post? && rack_request.env["CONTENT_TYPE"] == THRIFT_HEADER
+      end
+
+      class RequestResult
+        attr_reader :response
+
+        def initialize(response)
+          @response = response
+          @response_sent = false
+        end
+
+        def mark_response_sent!
+          @response_sent = true
+        end
+
+        def response_sent?
+          @response_sent
+        end
+      end
+
+      class ResponseTransport < IOStreamTransport
+        def initialize(input, output)
+          super
+          @response_sent = false
+        end
+
+        def response_sent!
+          @response_sent = true
+        end
+
+        def write(buf)
+          return if @response_sent
+
+          super
+        end
+      end
+
+      class OnewayResponseProtocol < SimpleDelegator
+        def initialize(protocol, &callback)
+          super(protocol)
+          @callback = callback
+          @oneway_message = false
+        end
+
+        def read_message_begin
+          message_begin = __getobj__.read_message_begin
+          @oneway_message ||= message_begin[1] == MessageTypes::ONEWAY
+          message_begin
+        end
+
+        def read_message_end
+          __getobj__.read_message_end.tap do
+            if @oneway_message
+              @oneway_message = false
+              @callback.call
+            end
+          end
+        end
       end
 
     end
--- lib/rb/spec/thin_http_server_spec.rb
+++ lib/rb/spec/thin_http_server_spec.rb
@@ -19,7 +19,10 @@
 #
 
 require 'spec_helper'
+require 'net/http'
 require 'rack/test'
+require 'socket'
+require 'timeout'
 require 'thrift/server/thin_http_server'
 
 describe Thrift::ThinHTTPServer do
@@ -81,6 +84,7 @@ describe Thrift::ThinHTTPServer::RackApplication do
 
   let(:processor) { double('processor') }
   let(:protocol_factory) { double('protocol factory') }
+  let(:protocol) { FakeProtocol.new }
 
   def app
     Thrift::ThinHTTPServer::RackApplication.for("/", processor, protocol_factory)
@@ -102,19 +106,19 @@ describe Thrift::ThinHTTPServer::RackApplication do
 
   context "200 response" do
     before do
-      allow(protocol_factory).to receive(:get_protocol)
+      allow(protocol_factory).to receive(:get_protocol).and_return(protocol)
       allow(processor).to receive(:process)
     end
 
     it 'creates an IOStreamTransport' do
       header('Content-Type', "application/x-thrift")
-      expect(Thrift::IOStreamTransport).to receive(:new).with(an_instance_of(Rack::Lint::InputWrapper), an_instance_of(Rack::Response))
+      expect(Thrift::ThinHTTPServer::RackApplication::ResponseTransport).to receive(:new).with(an_instance_of(Rack::Lint::InputWrapper), an_instance_of(Rack::Response)).and_call_original
       post "/"
     end
 
     it 'fetches the right protocol based on the Transport' do
       header('Content-Type', "application/x-thrift")
-      expect(protocol_factory).to receive(:get_protocol).with(an_instance_of(Thrift::IOStreamTransport))
+      expect(protocol_factory).to receive(:get_protocol).with(an_instance_of(Thrift::ThinHTTPServer::RackApplication::ResponseTransport))
       post "/"
     end
 
@@ -122,6 +126,269 @@ describe Thrift::ThinHTTPServer::RackApplication do
       header('Content-Type', "application/x-thrift")
       post "/"
       expect(last_response.ok?).to be true
+    end
+
+    it 'returns an empty response for oneway requests' do
+      allow(processor).to receive(:process) do |iprot, _oprot|
+        iprot.read_message_begin
+        iprot.read_message_end
+      end
+
+      header('Content-Type', "application/x-thrift")
+      post "/"
+
+      expect(last_response.ok?).to be true
+      expect(last_response.headers['Content-Length']).to eq('0')
+      expect(last_response.body).to eq('')
+    end
+
+    it 'sends an async oneway response before processing completes' do
+      response_queue = Queue.new
+      finish_queue = Queue.new
+      request = Rack::Request.new(
+        'rack.input' => StringIO.new(''),
+        'REQUEST_METHOD' => 'POST',
+        'CONTENT_TYPE' => 'application/x-thrift'
+      )
+
+      allow(processor).to receive(:process) do |iprot, _oprot|
+        iprot.read_message_begin
+        iprot.read_message_end
+        finish_queue.pop
+      end
+
+      thread = described_class.process_async_request(
+        request,
+        processor,
+        protocol_factory,
+        proc { |response| response_queue << response }
+      )
+
+      response = Timeout.timeout(1) { response_queue.pop }
+      expect(response).to eq([200, {'Content-Type' => 'application/x-thrift', 'Content-Length' => '0'}, []])
+      expect(thread).to be_alive
+
+      finish_queue << true
+      thread.join(1)
+      expect(thread).not_to be_alive
+    end
+
+    it 'detects oneway message headers for supported HTTP protocols' do
+      [
+        Thrift::BinaryProtocol,
+        Thrift::CompactProtocol,
+        Thrift::JsonProtocol
+      ].each do |protocol_class|
+        trans = Thrift::MemoryBufferTransport.new
+        protocol_class.new(trans).tap do |oprot|
+          oprot.write_message_begin('testOneway', Thrift::MessageTypes::ONEWAY, 1)
+          oprot.write_message_end
+        end
+        payload = trans.instance_variable_get(:@buf).dup
+
+        detected_type = nil
+        itrans = Thrift::MemoryBufferTransport.new(payload)
+        iprot = described_class::OnewayResponseProtocol.new(protocol_class.new(itrans)) do
+          detected_type = Thrift::MessageTypes::ONEWAY
+        end
+
+        iprot.read_message_begin
+        iprot.read_message_end
+        expect(detected_type).to eq(Thrift::MessageTypes::ONEWAY)
+      end
+    end
+
+    it 'returns immediately from a real Thin server while the oneway handler continues' do
+      processor = BlockingOnewayProcessor.new
+      port = free_port
+      server = Thrift::ThinHTTPServer.new(
+        processor,
+        :ip => '127.0.0.1',
+        :port => port,
+        :protocol_factory => Thrift::BinaryProtocolFactory.new
+      )
+      server_thread = Thread.new { server.serve }
+
+      wait_until_http_server_listens(port)
+
+      start = Time.now
+      response = Net::HTTP.post(
+        URI("http://127.0.0.1:#{port}/"),
+        oneway_request_body,
+        'Content-Type' => 'application/x-thrift'
+      )
+      elapsed = Time.now - start
+
+      expect(response.code).to eq('200')
+      expect(response.body).to eq('')
+      expect(elapsed).to be < 0.2
+      expect(processor.finished?).to be false
+
+      processor.finish
+      server.instance_variable_get(:@server).stop
+      server_thread.join(1)
+      expect(processor.finished?).to be true
+    ensure
+      processor&.finish
+      server&.instance_variable_get(:@server)&.stop
+      server_thread&.join(1)
+    end
+
+    it 'keeps a persistent HTTP connection usable after an early oneway response' do
+      processor = PersistentOnewayProcessor.new
+      port = free_port
+      server = Thrift::ThinHTTPServer.new(
+        processor,
+        :ip => '127.0.0.1',
+        :port => port,
+        :protocol_factory => Thrift::BinaryProtocolFactory.new
+      )
+      server_thread = Thread.new { server.serve }
+
+      wait_until_http_server_listens(port)
+
+      socket = TCPSocket.new('127.0.0.1', port)
+      socket.write(http_request(oneway_request_body))
+      first_response = read_http_response(socket)
+      expect(first_response).to include("HTTP/1.1 200 OK")
+      expect(first_response).to include("Content-Length: 0")
+      expect(processor.finished_count).to eq(0)
+
+      socket.write(http_request(oneway_request_body))
+      second_response = read_http_response(socket)
+      expect(second_response).to include("HTTP/1.1 200 OK")
+      expect(second_response).to include("Content-Length: 0")
+
+      processor.finish_all
+      server.instance_variable_get(:@server).stop
+      server_thread.join(1)
+      expect(processor.finished_count).to eq(2)
+    ensure
+      socket&.close
+      processor&.finish_all
+      server&.instance_variable_get(:@server)&.stop
+      server_thread&.join(1)
+    end
+  end
+
+  def free_port
+    socket = TCPServer.new('127.0.0.1', 0)
+    socket.addr[1]
+  ensure
+    socket&.close
+  end
+
+  def wait_until_http_server_listens(port)
+    Timeout.timeout(2) do
+      begin
+        Net::HTTP.get_response(URI("http://127.0.0.1:#{port}/"))
+      rescue Errno::ECONNREFUSED
+        sleep 0.01
+        retry
+      end
+    end
+  end
+
+  def oneway_request_body
+    trans = Thrift::MemoryBufferTransport.new
+    protocol = Thrift::BinaryProtocol.new(trans)
+    protocol.write_message_begin('testOneway', Thrift::MessageTypes::ONEWAY, 1)
+    protocol.write_struct_begin('TestOnewayArgs')
+    protocol.write_field_stop
+    protocol.write_struct_end
+    protocol.write_message_end
+    trans.instance_variable_get(:@buf).dup
+  end
+
+  def http_request(body)
+    [
+      "POST / HTTP/1.1",
+      "Host: 127.0.0.1",
+      "Content-Type: application/x-thrift",
+      "Content-Length: #{body.bytesize}",
+      "Connection: keep-alive",
+      "",
+      body
+    ].join("\r\n")
+  end
+
+  def read_http_response(socket)
+    Timeout.timeout(2) do
+      buffer = ''.dup
+      buffer << socket.readpartial(1024) until buffer.include?("\r\n\r\n")
+      headers, rest = buffer.split("\r\n\r\n", 2)
+      content_length = headers[/Content-Length:\s*(\d+)/i, 1].to_i
+      while rest.bytesize < content_length
+        rest << socket.readpartial(content_length - rest.bytesize)
+      end
+      "#{headers}\r\n\r\n#{rest}"
+    end
+  end
+
+  class FakeProtocol
+    attr_reader :trans
+
+    def initialize(message_type = Thrift::MessageTypes::ONEWAY)
+      @message_type = message_type
+    end
+
+    def read_message_begin
+      ['testOneway', @message_type, 1]
+    end
+
+    def read_message_end
+    end
+  end
+
+  class BlockingOnewayProcessor
+    def initialize
+      @finish_queue = Queue.new
+      @finished = false
+    end
+
+    def process(iprot, _oprot)
+      iprot.read_message_begin
+      iprot.read_struct_begin
+      _name, field_type, _field_id = iprot.read_field_begin
+      raise "expected empty args struct" unless field_type == Thrift::Types::STOP
+      iprot.read_struct_end
+      iprot.read_message_end
+      @finish_queue.pop
+      @finished = true
+      true
+    end
+
+    def finish
+      @finish_queue << true
+    end
+
+    def finished?
+      @finished
+    end
+  end
+
+  class PersistentOnewayProcessor
+    attr_reader :finished_count
+
+    def initialize
+      @finish_queue = Queue.new
+      @finished_count = 0
+    end
+
+    def process(iprot, _oprot)
+      iprot.read_message_begin
+      iprot.read_struct_begin
+      _name, field_type, _field_id = iprot.read_field_begin
+      raise "expected empty args struct" unless field_type == Thrift::Types::STOP
+      iprot.read_struct_end
+      iprot.read_message_end
+      @finish_queue.pop
+      @finished_count += 1
+      true
+    end
+
+    def finish_all
+      2.times { @finish_queue << true }
     end
   end
 end
